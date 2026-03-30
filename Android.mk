371f372ccb0dfba38a03ba83b03301f20f6da351
Tuan Anh <tuan73176@gmail.com>
2026-03-30 03:36:59 +0000

    DRG: Extract firmware
    Nokia/Dragon_00WW/DRG_sprout:10/QKQ1.190828.002/00WW_4_15T:user/release-keys

--- Android.mk
+++ Android.mk
@@ -3,3 +3,27 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),DRG)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,b5af07a333f77ed8163952c4d06dcf2ebbf16fc5)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,7d6393173a8ce5fae1c03364998690423de22c0f)
+$(call add-radio-file-sha1-checked,radio/cda.img,ffc2540b76c67d5d88a366af95786de4d9d28172)
+$(call add-radio-file-sha1-checked,radio/cmnlib.img,6b4c2f4a5cb1bfb8d0b79d1e047494c36b11737a)
+$(call add-radio-file-sha1-checked,radio/cmnlib64.img,7e301ee7e5960fb17ebdbc9d0036062d6c2907b6)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,4204618c12d8656046ed09a175f2a5aa650fa466)
+$(call add-radio-file-sha1-checked,radio/dsp.img,c991a83a1ea3e6a0c1572f3f8af8692c9a7b7cc1)
+$(call add-radio-file-sha1-checked,radio/hidden.img,76c2aed2c1bc34177c9a1e615c5938bbb6e388cd)
+$(call add-radio-file-sha1-checked,radio/hyp.img,344e54798484b735036e55ad94ec2b3eb606eee5)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,212e12ca0d1b3183e9eda31a3d0d9759d4c79d41)
+$(call add-radio-file-sha1-checked,radio/mdtp.img,b186389906aa96087fe3b6e0f2bcc1d4054bd02d)
+$(call add-radio-file-sha1-checked,radio/mdtpsecapp.img,dd21a2ba68226c22458c5467179a49de58f4e874)
+$(call add-radio-file-sha1-checked,radio/modem.img,26323c043a3344edba6a554b13622ee36e087d85)
+$(call add-radio-file-sha1-checked,radio/nvdef.img,a1fecd7672a782be019054d62d686cf912bb8a56)
+$(call add-radio-file-sha1-checked,radio/pmic.img,4f43537eb07e6ed117f8388d83e7fd2e9a1b0531)
+$(call add-radio-file-sha1-checked,radio/rpm.img,abbece7d734f7f821112b0614473915b9abda99e)
+$(call add-radio-file-sha1-checked,radio/systeminfo.img,bc1dffbe91576e92023d505738866eecd107de30)
+$(call add-radio-file-sha1-checked,radio/tz.img,463988b14cb445c5d048cb7d0855f5d35ed0e276)
+$(call add-radio-file-sha1-checked,radio/xbl.img,a49b905e500283922d1ab050aaaf83b26ed21113)
+
+endif
